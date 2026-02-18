2fa602df8fe6cc438e6cc31345e84cf2a853d2ef
foli3s <tgoldmaster@yandex.ru>
2026-02-18 22:36:50 +0700

    Add files via upload

---
+++ GitHubRepo/src/main/java/com/example/liopleurodon/LiopleurodonMod.java
@@ -0,0 +1,16 @@
+package com.example.liopleurodon;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("liopleurodonmod")
+public class LiopleurodonMod {
+    public static final String MOD_ID = "liopleurodonmod";
+    
+    public LiopleurodonMod() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
